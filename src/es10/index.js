4f0f69612affcdc7812d34f8e0d0ac46aa812201
wiskani <wiskani@icloud.com>
2022-01-20 22:27:42 -0400

    terminando

--- src/es10/index.js
+++ src/es10/index.js
@@ -1,4 +1,13 @@
 // array.flat
 let array=[1,2,3, [1,2,3, [1,2,3]]];
 
-console.log(array.flat())
+console.log(array.flat(2))
+
+//objetos entries
+let entries=[['name', 'oscar'],['age',32]];
+console.log(Object.fromEntries(entries));
+
+//simbolos acceder
+let mySymbl='My Symbol';
+let symbol=Symbol(mySymbl)
+console.log(symbol.description)
